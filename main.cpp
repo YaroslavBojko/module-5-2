78121c201aeba4b0a9086f47c240da3e3def19c9
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2024-01-08 13:56:49 +0300

    initial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+   int day;
+   std::cout << "Enter the day number: ";
+   std::cin >> day;
+
+   if (day % 7 == 0 || day % 7 == 6 || (day >= 1 && day <=5) || (day >= 8 && day <= 10))
+       std::cout << "Day off!!!" << std::endl;
+   else
+       std::cout << "Working day!!!" << std::endl;
+
 }
